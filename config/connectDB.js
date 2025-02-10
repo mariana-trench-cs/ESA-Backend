ff99c708b6952dd9c6ff980081392839d8c32c35
Arun_k19 <arunk1912005@gmail.com>
2025-02-10 10:50:04 +0530

    first

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const connectDB = async () => {'http://localhost:3002'
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useUnifiedTopology: true,
